201a34111c01a1063cef34f3c481b76a14afb2e5
Tommy Phan <tommyphan8@csu.fullerton.edu>
2015-03-08 21:14:35 -0700

    Finished API EXAMPLE for movie search

---
+++ Chapter5/ReadingExample/API_EXAMPLE/app.js
@@ -0,0 +1,29 @@
+var main = function() {
+	"use strict";
+	var $film;
+	var url = "http://api.themoviedb.org/3/search/movie?" +
+	"api_key=ae22cfb91b3d2c0a8179d99a29831489&query="
+	
+	$(".comment-input button").on("click", function (event){
+		$film = $(".comment-input input").val();
+		url = url + $film
+
+		$.getJSON(url, function (movieDB){
+			console.log(movieDB.results[0]);
+			movieDB.results.forEach(function(movie){
+				var $p = $("<li>").hide();
+				$p.text(movie.original_title);
+				$("body ul").append($p);
+				$p.fadeIn();
+				console.log(movie.original_title);
+
+			});
+
+		});
+
+	});
+	
+
+};
+
+$(document).ready(main);
